9fdd4e2df262f6db7b76f4215787194dfb1f1219
won lee <wonlee@wons-MacBook-Pro.local>
2024-02-02 01:37:24 +0900

    testing

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 function main() {
 
-  (function () {
-      'use strict';
-    /* ==============================================
-       Page Scrolling
-       =============================================== */
-
-      $('a.scroll-nav').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-              if (target.length) {
-                  $('html,body').animate({
-                      scrollTop: target.offset().top - 40
-                  }, 900);
-                  return false;
-              }
-          }
-      });
-
-      /*====================================
-       Show Menu on Book
-       ======================================*/
-      $(window).bind('scroll', function() {
-          var navHeight = $('#home').height() - 350;
-          if ($(window).scrollTop() > navHeight) {
-              $('.navbar-default').addClass('on');
-          } else {
-              $('.navbar-default').removeClass('on');
-          }
-      });
-
-      $(window).bind('scroll', function() {
-          var navHeight = $('#home').height() - 350;
-          if ($(window).scrollTop() > navHeight) {
-              $('.side').removeClass('on');
-          } else {
-              $('.side').removeClass('on');
-          }
-      });
+    (function () {
+        'use strict';
       /* ==============================================
-       Scroll Spy
-       =============================================== */
-      $('body').scrollspy({
-          target: '.navbar-default',
-          offset: 80
-      })
-   
-     }());
-    }
+         Page Scrolling
+         =============================================== */
+
+        $('a.scroll-nav').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        });
+
+        /*====================================
+         Show Menu on Book
+         ======================================*/
+        $(window).bind('scroll', function() {
+            var navHeight = $('#home').height() - 350;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+
+        $(window).bind('scroll', function() {
+            var navHeight = $('#home').height() - 350;
+            if ($(window).scrollTop() > navHeight) {
+                $('.side').removeClass('on');
+            } else {
+                $('.side').removeClass('on');
+            }
+        });
+        /* ==============================================
+         Scroll Spy
+         =============================================== */
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        })
+     
+       }());
+      }
 main();
